abd257e38c54e3976e0b20c933656864bbd94b88
okonji emmanuel <ebukeokonji@gmail.com>
2018-12-18 13:57:13 +0100

    feature(design-layouts): design layouts for fragments

--- app/src/main/java/com/andela/d2_news_application/ui/home/HomeFragment.kt
+++ app/src/main/java/com/andela/d2_news_application/ui/home/HomeFragment.kt
@@ -4,11 +4,13 @@ package com.andela.d2_news_application.ui.home
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.andela.d2_news_application.R
+import com.andela.d2_news_application.adapter.CommonAdapter
 import com.andela.d2_news_application.databinding.FragmentHomeBinding
 
 
@@ -20,10 +22,25 @@ class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
 
+    private val listAdapter by lazy {
+        CommonAdapter()
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false )
+        binding = DataBindingUtil.inflate(inflater,
+                R.layout.fragment_home,
+                container, false )
+
+        with(binding.homeRecycler) {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(
+                    context,
+                    LinearLayoutManager.VERTICAL,
+                    false)
+            adapter = listAdapter
+        }
         return binding.root
     }
 
